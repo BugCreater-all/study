dd34ae9e1b1fea94922dcc86a9698d49f7e4ab0d
BugCreater-all <2890824960@qq.com>
2020-09-06 21:42:31 +0800

    学习

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 1.cpp
+	> Author:sundong 
+	> Mail:2890824960@qq.com 
+	> Created Time: Sun Sep  6 21:39:48 2020
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    cout << 2 << endl;
+    return 0;
+}
